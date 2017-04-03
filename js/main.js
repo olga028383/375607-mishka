f38c5fb4f6bf64237f476273357f9a7fad3298c5
Olga <olga028383@mail.ru>
2017-04-03 15:19:55 +0300

    travis

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-function Popup(options){
+function Popup(options) {
   this.popup = document.querySelector('.popup-cart');
-  if(!this.popup) return;
+  if (!this.popup)
+    return;
   this.btn = options.btn;
   this.close = document.querySelector('.popup-form__btn');
-  
   this.showPopup = this.showPopup.bind(this);
   this.hidePopup = this.hidePopup.bind(this);
-  
-  for(var i = 0; i<this.btn.length; i++){
+
+  for (var i = 0; i < this.btn.length; i++) {
     this.btn[i].addEventListener('click', this.showPopup);
   }
-  
+
   this.close.addEventListener('click', this.hidePopup);
 }
 
-Popup.prototype.showPopup = function(e){
+Popup.prototype.showPopup = function (e) {
   e.preventDefault();
   this.popup.classList.add('popup-cart--show');
 };
 
-Popup.prototype.hidePopup = function(e){
+Popup.prototype.hidePopup = function (e) {
   e.preventDefault();
   this.popup.classList.remove('popup-cart--show');
 };
 
 
-function MenuMobile(){
+function MenuMobile() {
   this.menu = document.querySelector('.nav-menu');
   this.btn = this.menu.querySelector('.nav-menu__button');
-  
+
   this.menu.classList.remove('nav-menu--nojs');
   this.menu.classList.add('nav-menu--close');
-  
+
   this.toggleMenu = this.toggleMenu.bind(this);
-  
+
   this.btn.addEventListener('click', this.toggleMenu);
 }
 
-MenuMobile.prototype.toggleMenu = function(){
+MenuMobile.prototype.toggleMenu = function () {
   this.menu.classList.toggle('nav-menu--close');
 };
 
--- js/map.js
+++ js/map.js
@@ -1,19 +1,19 @@
-;(function(){
-	var myMap; 
+;
+(function () {
+  var myMap;
+  function init() {
+    myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055, 59.938631, 30.323055],
+      zoom: 14
+    });
+    var myPlacemark = new ymaps.Placemark(
+            [59.938631, 30.323055, 59.938631, 30.323055]
+            );
+    myMap.geoObjects.add(myPlacemark);
+  }
 
-function init(){ 
-	myMap = new ymaps.Map("map", {
-		center: [59.938631,30.323055, 59.938631,30.323055],
-		zoom: 14
-	}); 
-	var myPlacemark = new ymaps.Placemark(
-		[59.938631,30.323055, 59.938631,30.323055]        
-	);
-	myMap.geoObjects.add(myPlacemark); 
-}
-
-if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-	ymaps.ready(init);
-}
+  if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+    ymaps.ready(init);
+  }
 
 })();
